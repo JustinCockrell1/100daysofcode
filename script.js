63c6edbcccee06c9d46c546f090a876ddee9e4c1
Justin Cockrell <justin.cockrell1@gmail.com>
2022-04-21 02:46:54 -0500

    day 15

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const navigationIcon = document.querySelector(".navigation-button i");
 const navigationContent = document.querySelector(".navigation-content");
 
 let day = 1;
-let numDays=14;
+let numDays=15;
 
 let savedDay = localStorage.getItem("day");
 if(savedDay!=null) {
